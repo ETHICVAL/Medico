01c6f1ce9b48b8191e6906c78f511b84d074879f
code-lover636 <aravindashokan636@gmail.com>
2024-04-26 17:26:48 +0530

    Add Hero Section

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ menuBtn.addEventListener("click", e => {
     navBar.toggle("open");
     console.log(navBar);
 });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
